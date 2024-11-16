d595497655def35ba49bfa67b70d8fa82badcc54
rohitahirwal <ahirwar0206@gmail.com>
2024-11-16 23:40:13 +0530

    create resend_verifylink api

--- routers/basicrouter.js
+++ routers/basicrouter.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 
 const ApiResponse = require('./helpers/apiresponse.js')
 const {User,DonorInfo, sequelize} =  require('../models')
-const {check_password} = require('./helpers/validators.js')
+const {check_password, password_bcrypt, varify_password} = require('./helpers/password.js')
 const {generate_token,token_varify} = require('../config/generate_varify_token.js')
 const { email_fun } = require('../config/email.js')
 const { htmlfun, htmlfun2 } = require('../config/mail_design.js')
@@ -59,13 +59,12 @@ router.post('/donor_reg',async(req,res)=>{
 })
 
 
-
 router.put('/varify_email/:token',async(req,res)=>{
         
         const t = await sequelize.transaction()
         const token = req.params.token
         if(!token) {
-           return res.json(new ApiResponse(false,'No Token Found'))
+           return res.status(400).json(new ApiResponse(false,'No Token Found'))
         }
 
     try {
@@ -78,15 +77,20 @@ router.put('/varify_email/:token',async(req,res)=>{
             const userid = tokendata.userid
 
             const {password,DOB,loc_lat,loc_long,tracker_area} = req.body
-            if(check_password(password)) {
+
+            if(!check_password(password)) {
                return res.json(new ApiResponse(false,"password must be between 6 and 14 characters long"))
             }
-            
+            const hashpass = await password_bcrypt(password)
+           
             const user = await User.findOne({where:{id:userid}})
             if(!user) {
                return res.json(new ApiResponse(false,"User Not found"))
             }
-            const userdata = {password,status:true}
+            if(user.status) {
+                 return res.json(new ApiResponse(false,"User is already Registered and Verify "))
+            }
+            const userdata = {hashpass,status:true}
             await user.update(userdata,{transaction:t})
             await DonorInfo.update({DOB,loc_lat,loc_long,tracker_area,},{where:{user:userid}},{transaction:t})
             await t.commit()
@@ -95,38 +99,83 @@ router.put('/varify_email/:token',async(req,res)=>{
        
     } catch (error) {
             await t.rollback()
-            return res.json(new ApiResponse(false,"failed email varified"))
+            console.error('error :',error.message)
+            return res.json(new ApiResponse(false,"email varification failed"))
        }           
 })
 
 
 router.post('/login',async(req,res)=>{
 
+    const{email,password} = req.body
+    if(!check_password(password)) {
+       return res.json(new ApiResponse(false,'password must be between 7 and 14 characters long'))
+    }
+    
     try {
 
-        const{email,password} = req.body
-
-        if(check_password(password)) {
-            res.json(new ApiResponse(false,'password must be between 6 and 14 characters long'))
+        const user = await User.findOne({where:{email}})
+        if (!user) {
+            return res.status(404).json(new ApiResponse(false, 'User not found'));
+        }
+        if(!user.status) {
+             return res.status(403).json(new ApiResponse(false,'Account is not Activated'))
         }
 
-        const user = await User.findOne({where:{email,password}})
-        if(!(user || user.status)) {
-            res.json(new ApiResponse(false,'Inavlid Email or Password'))
+        const ispass_correct = await varify_password(password,user.password)
+        if(!ispass_correct) {
+            return res.status(401).json(new ApiResponse(false,'Incorrect password'))
         }
 
         const token = generate_token(user.id,user.role)
         const userid = user.id
         const role = user.role
-        res.json(new ApiResponse(true,"Correct Email And Password",{token,userid,role}))
-
+        return res.json(new ApiResponse(true,"Correct Email And Password",{token,userid,role}))
+        
     } catch (error) {
-         res.json(new ApiResponse(false,'Failed Login',error.message))
+        console.error('error:',error)
+         res.status(500).json(new ApiResponse(false,'Login failed',error.message))
     }
 
 })
 
 
+router.post('/resend_verifylink',async(req,res)=>{
+        const {email} = req.body
+
+        if (!email || !/\S+@\S+\.\S+/.test(email)) {
+            return res.status(400).json(new ApiResponse(false, "Invalid email format"));
+        }
+        const user = await User.findOne({where:{email}})
+        if(!user) {
+            return res.status(404).json(new ApiResponse(false,"User Invalid"))
+        }
+        if(user.status) {
+            return res.status(400).json(new ApiResponse(false,"User Already Verified"))
+        }
+
+        const tokenValidityDuration = 15 * 60 * 1000; // 15 minutes
+        const currentTime = new Date().getTime();
+        const lastSentTime = new Date(user.lastVerificationSent).getTime();
+
+        if (lastSentTime && currentTime - lastSentTime < tokenValidityDuration) {
+             return res.status(400).json(new ApiResponse(false, "Token is still valid. No need to resend."));
+        }
+
+    try {
+            
+            const token = generate_token(user.id,user.roll) 
+            const html_2 = htmlfun2(token,user.name)
+            await  email_fun(email,html_2,'Pulse Donar Verification')
+
+            user.lastVerificationSent = new Date();
+            await user.save();
+            return res.status(200).json(new ApiResponse(true,"Verification link sent. Please verify your email."))
+
+    } catch (error) {
+         res.status(500).json(new ApiResponse(false,"Failed to resend verification link. Please try again later."))    
+    }    
+})
 
 
 
